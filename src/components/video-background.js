462415a3ff930425f1728fd3e2c59b8cf55a9e83
Emmanuel C. Nsanga <admin@ai10x.dev>
2026-01-08 14:51:54 +1100

    add video

--- src/components/video-background.js
+++ src/components/video-background.js
@@ -11,7 +11,7 @@ const BackgroundVideo = () => {
                 className="video-background-element"
             >
                 <source
-                    src="https://assets.mixkit.co/videos/preview/mixkit-stars-in-the-night-sky-loop-1002-large.mp4"
+                    src="https://assets.mixkit.co/videos/4809/4809-720.mp4"
                     type="video/mp4"
                 />
                 Your browser does not support the video tag.
