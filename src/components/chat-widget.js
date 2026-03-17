0f5148f39e7b00a93fc9096b8eca943bb51e51bd
Emmanuel C. Nsanga <admin@ai10x.dev>
2026-03-17 11:15:12 +1100

    fetch errors

--- src/components/chat-widget.js
+++ src/components/chat-widget.js
@@ -478,8 +478,7 @@ const ChatWidget = () => {
             //console.log("Transaction Hash:", tx.hash)
 
             setVerifiedAddress(address)
-            const userPrompt = inputValue
-
+            const userPrompt = inputValue.text
 
 
             // Notify actively.run via proxy after successful blockchain signing
